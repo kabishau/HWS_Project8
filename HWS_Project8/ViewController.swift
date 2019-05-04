7017037141334f34ab6bcac0eeb3c77b3d3318ac
Aleksey DreamLine <aleksey.kabishau@dreamline.com>
2019-05-04 12:34:30 -0400

    Initial Commit

---
+++ HWS_Project8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWS_Project8
+//
+//  Created by user153502 on 5/4/19.
+//  Copyright © 2019 user153502. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
